dfe3e837986987eb315687c31b4258484cc599e9
raoxusheng <rxs@grrdeMacBook-Pro.local>
2021-02-01 19:30:26 +0800

    new

--- LC240.java
+++ LC240.java
@@ -1,2 +1,11 @@
 public class LC240 {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int i = matrix.length - 1, j = 0;
+        while (i >= 0 && j < matrix[0].length) {
+            if (matrix[i][j] == target) return true;
+            else if (matrix[i][j] > target) i--;
+            else j++;
+        }
+        return false;
+    }
 }
